341d23da7b0e3c5ba30449d6934105617ffbe7d2
unknown <creativeapurba@gmail.com>
2023-08-20 21:43:50 +0530

    Demonstrate post & body parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require("body-parser")
 const app = express();
 const port = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
    // res.send("App is running")
@@ -9,7 +12,12 @@ app.get("/", (req, res) => {
 
 app.get("/home", (req, res) => {
    // res.send("This is home route")
-   res.sendFile(__dirname + "/demo.txt")
+   res.sendFile(__dirname + "/home.html")
+})
+
+app.post("/", (req, res)=>{
+   console.log(req.body.username);
+   res.send("Post received")
 })
 
 app.listen(port, () => {
