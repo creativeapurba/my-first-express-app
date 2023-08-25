72c7ab9653f19e3622bb20e24d312782d7b914d7
unknown <creativeapurba@gmail.com>
2023-08-25 11:55:29 +0530

    Do basic

--- index.js
+++ index.js
@@ -4,20 +4,37 @@ const app = express();
 const port = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
    // res.send("App is running")
-   res.sendFile(__dirname + "/index.html")
+   res.sendFile(__dirname + "/public/index.html")
 });
 
 app.get("/home", (req, res) => {
    // res.send("This is home route")
    res.sendFile(__dirname + "/home.html")
 })
+app.get("/redirect", (req, res)=>{
+   res.redirect("/home")
+})
+
+app.get("/multiline", (req, res)=>{
+   res.write("Line 1\n")
+   res.write("Line 2")
+   res.send()
+})
 
 app.post("/", (req, res)=>{
    console.log(req.body.username);
    res.send("Post received")
+})
+app.get("/status", (req, res)=>{
+   res.status(304).send("Status")
+})
+
+app.get("/html", (req, res)=>{
+   res.send("<h1 style='color:red'>Html</h1>")
 })
 
 app.listen(port, () => {
