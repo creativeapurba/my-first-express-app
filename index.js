3eb1bb73d0b12add2d5b9ed231f660360b761b67
unknown <creativeapurba@gmail.com>
2023-08-26 12:40:53 +0530

    EJS

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bodyParser = require("body-parser")
 const app = express();
 const port = 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
    // res.send("App is running")
@@ -15,35 +16,46 @@ app.get("/home", (req, res) => {
    // res.send("This is home route")
    res.sendFile(__dirname + "/home.html")
 })
-app.get("/redirect", (req, res)=>{
+app.get("/redirect", (req, res) => {
    res.redirect("/home")
 })
-app.get("/user",(req, res)=>{
-   res.send({name: "Omprakash"})
+app.get("/user", (req, res) => {
+   res.send({ name: "Omprakash" })
 })
 
-app.get("/multiline", (req, res)=>{
+app.get("/multiline", (req, res) => {
    res.write("Line 1\n")
    res.write("Line 2")
    res.send()
 })
 
-app.post("/", (req, res)=>{
+app.post("/", (req, res) => {
    console.log(req.body.username);
    res.send("Post received")
 })
-app.get("/status", (req, res)=>{
+app.get("/status", (req, res) => {
    res.status(304).send("Status")
 })
 
-app.get("/html", (req, res)=>{
+app.get("/html", (req, res) => {
    res.send("<h1 style='color:red'>Html</h1>")
 })
 
-app.get("/login", (req, res)=>{
+app.get("/login", (req, res) => {
    res.sendFile(__dirname + "/public/login.html")
+})
+
+app.get("/ejs", (req, res) => {
+   res.render("template.ejs", { name: "Omprakash", surname: "Biswas" })
+})
+
+app.get("/ejs2", (req, res)=>{
+   res.render("template2.ejs")
 })
 
 app.listen(port, () => {
    console.log("App is running");
 })
+
+
+// MERN => MONGODB EXPRESSJS REACT NODEJS
